eb39fb6aa7049169083a84c7263cce9d66974625
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-17 01:15:35 -0700

    Update imagine.py

--- src/modules/imagine.py
+++ src/modules/imagine.py
@@ -4,27 +4,25 @@ import time
 import os
 from src import app 
 
-url = 'https://ai-api.magicstudio.com/api/ai-art-generator'
-
-@app.on_message(filters.command("imagine", prefixes=["/", "!"]))
+@app.on_message(filters.command("segs", prefixes=["/", "!"]))
 async def draw_prompt(app, message):
-    prompt = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else "A Cat"
-    form_data = {
-        'prompt': prompt,
-        'output_format': 'bytes',
-        'request_timestamp': str(int(time.time())),
-        'user_is_subscribed': 'false',
-    }
-    response = requests.post(url, data=form_data)
-
+    query = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else "beautiful sunset"
+    
+    if query.startswith("http"):
+        response = requests.get(query)
+        image_data = response.content
+    else:
+        response = requests.get("https://source.unsplash.com/random/?" + query)
+        image_data = response.content
+    
     if response.status_code == 200:
         try:
-            if response.content:
+            if image_data:
                 destination_dir = ''
                 destination_path = os.path.join(destination_dir, 'generated_image.jpg')
                 
                 with open(destination_path, 'wb') as f:
-                    f.write(response.content)
+                    f.write(image_data)
                 
                 await app.send_photo(
                     chat_id=message.chat.id,
