6d9fc7f0564e708cad1ad5d4d4e03e4cee32fa11
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-17 01:13:40 -0700

    Update start.py

--- src/modules/start.py
+++ src/modules/start.py
@@ -1,22 +1,78 @@
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup 
+import asyncio
+import random
+import os
+
 from pyrogram import filters
-import config
-from config import OWNER_ID
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message, InlineKeyboardButton, CallbackQuery
 from src import app
-# Define a list to store sudo users
-sudo_users = []
 
-# Function to check if a user is a sudo user
-def is_sudo(user_id):
-    return user_id in sudo_users
 
-# Handle the /start command
 @app.on_message(filters.command("start"))
-async def strt(app, message: Message):
-    await message.reply_photo(
-        photo="https://graph.org/file/cf76e89dbee87a7ac0a76.jpg",
-        caption=f"{app.me.mention} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ.",
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{app.me.username}?startgroup=true")]
-        ])
+async def start(_, m: Message):
+
+    await m.reply_photo("https://graph.org/file/335375fb9e68fe30dd276.jpg", caption=f"""🥀 ʜᴇʏ {m.from_user.mention},\n\nᴛʜɪs ɪs {app.me.mention},\nᴛʜᴇ ᴍᴏsᴛ ᴜsᴇʟᴇss ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ᴇᴠᴇʀ ᴍᴀᴅᴇ.""",
+                         reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ", url=f"https://t.me/{app.me.username}?startgroup=new")],
+        [InlineKeyboardButton(text="ʜᴇʟᴘ", callback_data="help"), InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", callback_data="source")]
+    ]))
+
+@app.on_callback_query(filters.regex("help"))
+async def cb_func(_, query: CallbackQuery):
+    await query.message.edit_text(text=f"""➻ ʜᴇʀᴇ ɪs ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ ᴏғ {app.me.mention} :
+
+↬ /ai : ᴜꜱᴇ ꜰʀᴇᴇ ᴀɪ ᴡɪᴛʜᴏᴜᴛ ᴀᴘɪ ᴋᴇʏ.
+↬ /imagine : ɢɪᴠᴇ ᴘʀᴏᴍᴘᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ 𝟾ᴋ ᴀɪ ɪᴍᴀɢᴇs.
+↬ /mid : ɢɪᴠᴇ ᴘʀᴏᴍᴘᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴍɪᴅ ᴀɪ ɪᴍᴀɢᴇs                                                                                              
+↬ /alive : sʜᴏᴡs ᴛʜᴇ ᴀʟɪᴠᴇ sᴛᴀᴛᴜs ᴏғ ᴛʜᴇ ʙᴏᴛ.
+↬ /echo : sᴇɴᴅs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
+↬ /figlet : ɢᴇɴᴇʀᴀᴛᴇs ᴀ ғɪɢʟᴇᴛ ᴏғ ɢɪᴠᴇɴ ɴᴀᴍᴇ.
+↬ /img : sʜᴏᴡs ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ɪᴍᴀɢᴇ.
+↬ /ping : sʜᴏᴡs ᴛʜᴇ ᴘɪɴɢ ʟᴀᴛᴇɴᴄʏ ᴀɴᴅ sʏsᴛᴇᴍ sᴛᴀᴛs ᴏғ ᴛʜᴇ ʙᴏᴛ.
+↬ /q : ᴍᴀᴋᴇ's ᴀ ǫᴜᴏᴛᴇ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ.
+↬ /shorturl : sʜᴏʀᴛᴇɴs ᴛʜᴇ ɢɪᴠᴇɴ ᴜʀʟ.
+↬ /start : sᴛᴀʀᴛs ᴍᴇ.
+↬ /sname : ᴅᴇᴄᴏᴅᴇs ᴛʜᴇ ɴᴀᴍᴇ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ᴡɪᴛʜ ᴜɴɪᴅᴇᴄᴏᴅᴇ.
+↬ /webss : ᴄᴀᴘᴛᴜʀᴇs ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ sɪᴛᴇ.""", 
+                                  reply_markup=InlineKeyboardMarkup([
+                                      [InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="back")]]))
+
+
+@app.on_callback_query(filters.regex("back"))
+async def cb_func(_, query: CallbackQuery):
+    await query.message.edit_text(text=f"""🥀 ʜᴇʏ {query.from_user.mention},\n\nᴛʜɪs ɪs {app.me.mention},\nᴛʜᴇ ᴍᴏsᴛ ᴜsᴇʟᴇss ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ᴇᴠᴇʀ ᴍᴀᴅᴇ.""",        
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ", url=f"https://t.me/{app.me.username}?startgroup=new")
+                    ],
+                    [
+                        InlineKeyboardButton(text="ʜᴇʟᴘ", callback_data="help"),
+                        InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", callback_data="source")
+                    ]
+                ]
+            )
+    )
+
+
+@app.on_callback_query(filters.regex("source"))
+async def cb_func(_, query: CallbackQuery):
+    await query.message.edit_text(text=f"""ғᴏʀ sᴏᴜʀᴄᴇ ᴄᴏɴᴛᴀᴄᴛ @Miryze""",
+                                  reply_markup=InlineKeyboardMarkup([
+                                      [InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="back")]]))
+    
+@app.on_callback_query(filters.regex("back"))
+async def cb_func(_, query: CallbackQuery):
+    await query.message.edit_text(text=f"""🥀 ʜᴇʏ {query.from_user.mention},\n\nᴛʜɪs ɪs {app.me.mention},\nᴛʜᴇ ᴍᴏsᴛ ᴜsᴇʟᴇss ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ᴇᴠᴇʀ ᴍᴀᴅᴇ.""",        
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ", url=f"https://t.me/{app.me.username}?startgroup=new")
+                    ],
+                    [
+                        InlineKeyboardButton(text="ʜᴇʟᴘ", callback_data="help"),
+                        InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", callback_data="source")
+                    ]
+                ]
+            )
     )
