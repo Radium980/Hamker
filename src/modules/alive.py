7ae7409a9be950f12913190950c9f67d21c28cd8
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-18 13:30:51 -0700

    Update alive.py

--- src/modules/alive.py
+++ src/modules/alive.py
@@ -2,6 +2,17 @@ from pyrogram import Client, filters
 import psutil
 import datetime
 from src import app
+from pymongo import MongoClient
+from config import MONGO_DB_URI
+
+DATABASE = MongoClient(MONGO_DB_URI)
+db = DATABASE["MAIN"]["USERS"]
+collection = db["members"]
+
+def add_user_database(user_id: int):
+    check_user = collection.find_one({"user_id": user_id})
+    if not check_user:
+        return collection.insert_one({"user_id": user_id})
 
 def get_uptime():
     uptime_seconds = psutil.boot_time()
@@ -13,7 +24,7 @@ def get_uptime():
 
 @app.on_message(filters.command("alive", prefixes="/"))
 async def alive_command(client, message):
+    add_user_database(message.from_user.id)  # Add user to the database
     hours, minutes, seconds = get_uptime()
     uptime_message = f"I'ᴍ ᴀᴡᴀᴋᴇ sɪɴᴄᴇ {hours} ʜᴏᴜʀs, {minutes}  ᴍɪɴᴜᴛᴇs, {seconds} sᴇᴄᴏɴᴅs"
     await message.reply_text(uptime_message)
-
