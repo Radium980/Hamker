2e4b93ae699d501c6b9746c73195c5153409c246
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-18 13:32:18 -0700

    Update echo.py

--- src/modules/echo.py
+++ src/modules/echo.py
@@ -1,9 +1,21 @@
 from pyrogram import filters
 from html import escape, unescape
 from src import app
+from pymongo import MongoClient
+from config import MONGO_DB_URI
+
+DATABASE = MongoClient(MONGO_DB_URI)
+db = DATABASE["MAIN"]["USERS"]
+collection = db["members"]
+
+def add_user_database(user_id: int):
+    check_user = collection.find_one({"user_id": user_id})
+    if not check_user:
+        return collection.insert_one({"user_id": user_id})
 
 @app.on_message(filters.command("echo"))
 async def echo(app, message):
+    add_user_database(message.from_user.id)  # Add user to the database
     if message.reply_to_message:
         text = message.reply_to_message.text
         await message.reply_text(text)
