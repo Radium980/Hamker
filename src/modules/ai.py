b70c42fbb8bc7f7757b8e8d41c69d293e92a2c92
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-18 13:26:10 -0700

    Update ai.py

--- src/modules/ai.py
+++ src/modules/ai.py
@@ -4,6 +4,17 @@ import json
 import pyrogram
 from src import app
 import requests
+from pymongo import MongoClient
+from config import MONGO_DB_URI
+
+DATABASE = MongoClient(MONGO_DB_URI)
+db = DATABASE["MAIN"]["USERS"]
+collection = db["members"]
+
+def add_user_database(user_id: int):
+    check_user = collection.find_one({"user_id": user_id})
+    if not check_user:
+        return collection.insert_one({"user_id": user_id})
 
 def chat_with_api(model, prompt):
     url = f"https://tofu-api.onrender.com/chat/{model}/{prompt}"
@@ -19,5 +30,6 @@ def chat_with_api(model, prompt):
 
 @app.on_message(pyrogram.filters.command("ai"))
 async def gptAi(app: pyrogram.Client, m):
+    add_user_database(m.from_user.id)  # Add user to the database
     so = m.text.split(None, 1)[1]
     await m.reply(chat_with_api("gpt", so))
