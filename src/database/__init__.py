4e76e67e60d48fde8ab3e195d7248d8ac5163350
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-18 22:40:34 -0700

    Update __init__.py

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,5 @@
 import config
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-mongo = MongoClient(config.MONGO_DB_URL)
+mongo = MongoClient(config.MONGO_DB_URI)
 db = mongo.PythonCux
