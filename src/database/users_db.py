93c40b12021d99efee37dc13420815735c395816
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-04-18 22:33:21 -0700

    Create users_db.py

---
+++ src/database/users_db.py
@@ -0,0 +1,23 @@
+from config import MONDO_DB_URI
+
+db = MONDO_DB_URI
+usersdb = db.users
+
+async def get_served_users() -> list:
+    chats = await usersdb.find({"user_id": {"$gt": 0}}).to_list(length=None)
+    return [chat["user_id"] for chat in chats]
+
+async def is_served_user(chat_id: int) -> bool:
+    chat = await usersdb.find_one({"user_id": chat_id})
+    return bool(chat)
+
+
+async def add_served_user(user_id: int, username: str):
+    await usersdb.update_one(
+        {"user_id": user_id},
+        {"$set": {"username": username}},
+        upsert=True
+    )
+
+async def remove_served_user(chat_id: int):
+    await usersdb.delete_one({"user_id": chat_id})
