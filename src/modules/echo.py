b8b4b9c6b68f5e9ffed388f222c0b46355009670
Mirzya <126695903+Mirzye@users.noreply.github.com>
2024-04-23 08:35:59 -0700

    Update echo.py

--- src/modules/echo.py
+++ src/modules/echo.py
@@ -13,12 +13,11 @@ def add_user_database(user_id: int):
     if not check_user:
         return collection.insert_one({"user_id": user_id})
 
-@app.on_message(filters.command("echo"))
 async def echo(app, message):
     add_user_database(message.from_user.id)  # Add user to the database
     if message.reply_to_message:
-        text = message.reply_to_message.text
-        await message.reply_text(text)
+        text = message.text.split(0, 1)[1]
+        await app.send_message(chat_id=message.chat.id, text=text, reply_to_message_id=message.reply_to_message.id)
     else:
         try:
             text = message.text.split(maxsplit=1)[1]
