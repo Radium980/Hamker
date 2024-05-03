384e88fae1bc1d48a439837ca14ef747e81434c7
Mirzya <126695903+Mirzye@users.noreply.github.com>
2024-05-03 02:36:18 -0700

    Update stats.py

--- src/modules/stats.py
+++ src/modules/stats.py
@@ -4,7 +4,7 @@ from src.database.users_db import get_served_users
 from pyrogram import filters
 from pyrogram.types import Message
 
-@app.on_message(filters.command("stats"))
+@app.on_message(filters.command("stats", ["!", "/", "."]))
 async def unxstats(_, msg: Message):
     served_users = await get_served_users()
     users = (len(served_users))
