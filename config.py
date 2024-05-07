e5166818bc8e9b9b921276195b4e2f2d090c4ef3
Mirzya <126695903+Mirzye@users.noreply.github.com>
2024-05-07 11:06:53 -0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,5 @@ API_ID = int(getenv("API_ID", ))
 API_HASH = getenv("API_HASH", )
 BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
 LOG_ID = int(getenv("LOG_ID"))
-OWNER_ID = getenv("OWNER_ID", 6950368169)
 SUDOERS = list(map(int, getenv("SUDOERS", "").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-SESSION = getenv("SESSION", None)
